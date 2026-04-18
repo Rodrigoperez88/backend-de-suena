d4ed44b08b31f2efb4cecd5aa616c2f797f0726e
Rodrigo Perez <rodrigogastonperez1988@gmail.com>
2026-04-18 17:50:48 -0300

    Simplify Prisma connection for Render

--- prismaClient.js
+++ prismaClient.js
@@ -1,19 +1,12 @@
 import "dotenv/config";
 import prismaPkg from "@prisma/client";
-import { PrismaPg } from "@prisma/adapter-pg";
-import pkg from "pg";
 
 const { PrismaClient } = prismaPkg;
-const { Pool } = pkg;
 
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-});
+const prisma = globalThis.__prisma__ || new PrismaClient();
 
-const adapter = new PrismaPg(pool);
-
-const prisma = new PrismaClient({
-  adapter,
-});
+if (process.env.NODE_ENV !== "production") {
+  globalThis.__prisma__ = prisma;
+}
 
 export default prisma;
